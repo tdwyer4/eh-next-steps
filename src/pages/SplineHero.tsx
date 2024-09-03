146281956e293c9288b79e51ff2c922d30f64ee6
Tim Dwyer <timdwyer@Tims-MacBook-Pro.local>
2024-09-03 14:28:01 -0500

    Page Updates

--- src/pages/SplineHero.tsx
+++ src/pages/SplineHero.tsx
@@ -1,9 +1,12 @@
 import React from "react";
 import Spline from "@splinetool/react-spline";
+import "./SplineHero.css";
 
 function SplineHero() {
   return (
-    <Spline scene="https://prod.spline.design/ljBCZPfzU8ZWXhR2/scene.splinecode" />
+    <div className="splineTrack">
+      <Spline scene="https://prod.spline.design/ljBCZPfzU8ZWXhR2/scene.splinecode" />
+    </div>
   );
 }
 
